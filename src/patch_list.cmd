a3fb256fbcb3bb76805c688f1332ff77c9f01dae
p.lugovskov <p.lugovskov@gmail.com>
2021-03-09 19:17:14 +0300

    patch UEFI modules, replaced WiFi with new - command file

--- src/patch_list.cmd
+++ src/patch_list.cmd
@@ -2,3 +2,4 @@ bincmp Section_PE32_image_233DF097-3218-47B2-9E09-FE58C2B20D22_PlatformStage2_bo
 bincmp Section_PE32_image_53984C6A-1B4A-4174-9512-A65E5BC8B278_PlatformStage1_body.efi -patch patch_2928h.txt
 bincmp Section_PE32_image_5EE86B35-0839-4A21-8845-F1ACB0F688AB_WLAN_body.efi -patch patch_AE8h.txt
 bincmp Section_PE32_image_PlatformSetup_PlatformSetup_body.efi -patch patch_7AF8h.txt
+pause
