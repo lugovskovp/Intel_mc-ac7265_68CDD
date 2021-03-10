8529e1ced58cddf898802cfab2d029130585abf1
p.lugovskov <p.lugovskov@gmail.com>
2021-03-10 16:33:19 +0300

    EFI tables and structures

--- src/behemoth.h
+++ src/behemoth.h
@@ -21851,6 +21851,7 @@ typedef struct _EFI_PEI_SERVICES {
   EFI_PEI_REGISTER_FOR_SHADOW     RegisterForShadow;
 } EFI_PEI_SERVICES;
 
+typedef struct _EFI_PEI_SERVICES EFI_PEI_SERVICES ;
 //
 // JB: Some PEI structures
 //
