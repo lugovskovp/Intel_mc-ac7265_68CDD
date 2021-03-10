c7671c2bdad0b99d0688aba385efdd32284d5075
p.lugovskov <p.lugovskov@gmail.com>
2021-03-10 15:19:05 +0300

    command file - patcher saved UEFI modules, replac WiFi id with new

--- src/patch_list.cmd
+++ src/patch_list.cmd
@@ -1,5 +1,6 @@
-bincmp Section_PE32_image_233DF097-3218-47B2-9E09-FE58C2B20D22_PlatformStage2_body.efi -patch patch_30A8h.txt
-bincmp Section_PE32_image_53984C6A-1B4A-4174-9512-A65E5BC8B278_PlatformStage1_body.efi -patch patch_2928h.txt
-bincmp Section_PE32_image_5EE86B35-0839-4A21-8845-F1ACB0F688AB_WLAN_body.efi -patch patch_AE8h.txt
-bincmp Section_PE32_image_PlatformSetup_PlatformSetup_body.efi -patch patch_7AF8h.txt
+bincmp File_DXE_driver_5EE86B35-0839-4A21-8845-F1ACB0F688AB_WLAN.ffs -patch 5EE86B35-0839-4A21-8845-F1ACB0F688AB_patch.txt
+bincmp File_DXE_driver_PlatformSetup_PlatformSetup.ffs -patch F6D35FBB-63EA-4B25-81A5-5E62B4886292_PlatformSetup_patch.txt
+bincmp File_PEI_module_233DF097-3218-47B2-9E09-FE58C2B20D22_PlatformStage2.ffs -patch 233DF097-3218-47B2-9E09-FE58C2B20D22_PlatformStage2_patch.txt
+bincmp File_PEI_module_53984C6A-1B4A-4174-9512-A65E5BC8B278_PlatformStage1.ffs -patch 53984C6A-1B4A-4174-9512-A65E5BC8B278_PlatformStage1_patch.txt
 pause
+
